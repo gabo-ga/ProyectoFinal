8b29bee2109794abcebbdc089a139e22cbac2a4a
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2024-09-17 17:09:08 -0400

    feat:usuario model choices

--- Delivery/AngusEats/models.py
+++ Delivery/AngusEats/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    ROL_CHOICES = [
+        ('admin', 'Admin'),
+        ('operador', 'Operador'),
+        ('conductor', 'Conductor')
+    ]
+    
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField(unique=True)
+    contrasena_hash = models = models.CharField(max_length=64)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.nombre
+    
+    
