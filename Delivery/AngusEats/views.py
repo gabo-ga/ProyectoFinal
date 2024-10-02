43d4a781abff506ffb581bf3ddde20c925600c26
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2024-10-02 16:29:49 -0400

    feat:react element LoginCard

--- Delivery/AngusEats/views.py
+++ Delivery/AngusEats/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 # Create your views here.
+
+class ProtectedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        data = {'message': 'Esta es una vista protegida'}
+        return Response(data)
