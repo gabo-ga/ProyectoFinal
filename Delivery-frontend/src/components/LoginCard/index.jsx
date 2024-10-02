97338ef76af6cea13237e71ed63ab508807001d2
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2024-10-02 18:24:31 -0400

    feat:loginCard react

--- Delivery-frontend/src/components/LoginCard/index.jsx
+++ Delivery-frontend/src/components/LoginCard/index.jsx
@@ -1,46 +1,111 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  width: 100%;
+  padding: 15px;
+  background-color: #ffc107;
+  border: none;
+  color: #fff;
+  font-weight: bold;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+const Input = styled.input`
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #bdbdbd;
+  border-radius: 5px;
+  box-sizing: border-box;
+`;
+const Label = styled.label`
+  display: block;
+  margin-bottom: 5px;
+  color: #37474f;
+  font-weight: bold;
+`;
+const Container = styled.div`
+  margin-bottom: 20px;
+  text-align: left;
+`;
+const Body = styled.body`
+  margin: 0;
+  padding: 0;
+  font-family: Arial, sans-serif;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #00a794;
+`;
+const LoginContainer = styled.div`
+  width: 350px;
+  padding: 40px;
+  background-color: #fff;
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  text-align: center;
+`;
+const H1 = styled.h1`
+  margin-bottom: 30px;
+  font-size: 24px;
+  color: #37474f;
+`;
+
 function LoginCard() {
-    return (
-        <>
-        <div className="login-container">
-    <h1>INICIAR SESION</h1>
-    <div className="input-group">
-        <EmailInput></EmailInput>
-        <PasswordInput></PasswordInput>
-    </div>
-    <LoginButton></LoginButton>
-</div>   
-        </>
-    );
+  return (
+    <>
+      <Body>
+        <LoginContainer>
+          <H1>INICIAR SESION</H1>
+          <Container>
+            <EmailInput></EmailInput>
+            <PasswordInput></PasswordInput>
+          </Container>
+          <LoginButton></LoginButton>
+        </LoginContainer>
+      </Body>
+    </>
+  );
 }
 
-function EmailInput(){
-    return (
-        <>
-        <div>
-            <label for="email">Correo electrónico</label>
-            <input type="email" id="email" name="email" placeholder="Ingrese su correo"/>
-            </div>
-        </>
-    );
+function EmailInput() {
+  return (
+    <>
+      <Container>
+        <Label for="email">Correo electrónico</Label>
+        <Input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Ingrese su correo"
+        />
+      </Container>
+    </>
+  );
 }
 
-function PasswordInput(){
-    return(
-        <>
-        <div>
-        <label for="password">Contraseña</label>
-        <input type="password" id="password" name="password" placeholder="Ingrese su contraseña"/>
-        </div>
-        </>
-    );
+function PasswordInput() {
+  return (
+    <>
+      <div>
+        <Label for="password">Contraseña</Label>
+        <Input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Ingrese su contraseña"
+        />
+      </div>
+    </>
+  );
 }
 
-function LoginButton(){
-    return (
-        <>
-        <button className="btn-login">INICIAR SESION</button>
-        </>
-    );
+function LoginButton() {
+  return (
+    <>
+      <Button>INICIAR SESION</Button>
+    </>
+  );
 }
 
 export default LoginCard;
