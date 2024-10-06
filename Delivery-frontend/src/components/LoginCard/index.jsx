9b3bca87d20aa8578e97c7e189ca7455630a0a0f
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2024-10-06 16:59:41 -0400

    style:add bootstrap to login page

--- Delivery-frontend/src/components/LoginCard/index.jsx
+++ Delivery-frontend/src/components/LoginCard/index.jsx
@@ -1,88 +1,34 @@
-import styled from "styled-components";
-
-const Button = styled.button`
-  width: 100%;
-  padding: 15px;
-  background-color: #ffc107;
-  border: none;
-  color: #fff;
-  font-weight: bold;
-  border-radius: 5px;
-  cursor: pointer;
-`;
-const Input = styled.input`
-  width: 100%;
-  padding: 12px;
-  border: 1px solid #bdbdbd;
-  border-radius: 5px;
-  box-sizing: border-box;
-`;
-const Label = styled.label`
-  display: block;
-  margin-bottom: 5px;
-  color: #37474f;
-  font-weight: bold;
-`;
-const Container = styled.div`
-  margin-bottom: 20px;
-  text-align: left;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-
-  @media (min-width: 390px) {
-    width: 80%;
-  }
-`;
-const Body = styled.div`
-  margin: 0;
-  padding: 0;
-  font-family: Arial, sans-serif;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #1abc9c;
-`;
-const LoginContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  width: 25vw;
-  padding: 4vw;
-  background-color: #fff;
-  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.1);
-  border-radius: 10px;
-  text-align: center;
-
-  @media (min-width: 390px) {
-    width: 80%;
-    height: 70%;
-  }
-`;
-const H1 = styled.h1`
-  margin-bottom: 30px;
-  font-size: 1.75rem;
-  color: #37474f;
-
-  @media (min-width: 390px) {
-    font-size: 1.5rem;
-  }
-`;
+import styles from "./index.module.css";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function LoginCard() {
   return (
     <>
-      <Body>
-        <LoginContainer>
-          <H1>INICIAR SESION</H1>
-          <Container>
-            <EmailInput></EmailInput>
-            <PasswordInput></PasswordInput>
-          </Container>
-          <LoginButton></LoginButton>
-        </LoginContainer>
-      </Body>
+      <div className={styles.Background}>
+        <Container className={styles.FormContainer}>
+          <Row className={styles.RowStyle}>
+            <Col xs={12} md={8} lg={4}>
+              <Card>
+                <Card.Body>
+                  <Card.Title className={styles.LoginTitle}>
+                    INICIAR SESION
+                  </Card.Title>
+                  <Form>
+                    <EmailInput></EmailInput>
+                    <PasswordInput></PasswordInput>
+                    <LoginButton></LoginButton>
+                  </Form>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </>
   );
 }
@@ -90,13 +36,18 @@ function LoginCard() {
 function EmailInput() {
   return (
     <>
-      <Label htmlFor="email">Correo electrónico</Label>
-      <Input
-        type="email"
-        id="email"
-        name="email"
-        placeholder="Ingrese su correo"
-      />
+      <div>
+        <Form.Group controlId="email" className="mb-3">
+          <Form.Label className={styles.LabelStyle}>
+            Correo electrónico
+          </Form.Label>
+          <Form.Control
+            className={styles.InputStyle}
+            type="email"
+            placeholder="Ingrese su correo"
+          />
+        </Form.Group>
+      </div>
     </>
   );
 }
@@ -104,13 +55,16 @@ function EmailInput() {
 function PasswordInput() {
   return (
     <>
-      <Label htmlFor="password">Contraseña</Label>
-      <Input
-        type="password"
-        id="password"
-        name="password"
-        placeholder="Ingrese su contraseña"
-      />
+      <div>
+        <Form.Group controlId="password" className="mb-3">
+          <Form.Label className={styles.LabelStyle}>Contraseña</Form.Label>
+          <Form.Control
+            className={styles.InputStyle}
+            type="password"
+            placeholder="Ingrese su contraseña"
+          />
+        </Form.Group>
+      </div>
     </>
   );
 }
@@ -121,7 +75,16 @@ function LoginButton() {
   };
   return (
     <>
-      <Button onClick={handleClick}>INICIAR SESION</Button>
+      <div>
+        <Button
+          className={styles.LoginButton}
+          variant="warning"
+          type="submit"
+          onClick={handleClick}
+        >
+          INICIAR SESIÓN
+        </Button>
+      </div>
     </>
   );
 }
