87a9325afc9ab5084e81f0bdc7a80f133ff8f01c
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2024-10-06 17:34:11 -0400

    restframework and cors header intalles

--- Delivery/Delivery/settings.py
+++ Delivery/Delivery/settings.py
@@ -38,15 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
-    #'corsheaders',
+    'corsheaders',
     'AngusEats',
-    #'rest_framework',
+    'rest_framework',
 ]
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:5174",]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    #'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
--- Delivery/Delivery/urls.py
+++ Delivery/Delivery/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import ( TokenObtainPairView, TokenRefreshView,)
 
 urlpatterns = [
