a029e2ea97c0599670a5d15c46beba3fe6b99651
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2024-10-16 23:26:32 -0400

    refactor:login card refactored

--- Delivery-frontend/src/components/LoginCard/index.jsx
+++ Delivery-frontend/src/components/LoginCard/index.jsx
@@ -1,12 +1,13 @@
 import styles from "./index.module.css";
-import Button from "react-bootstrap/Button";
-import Alert from "react-bootstrap/Alert";
-import Card from "react-bootstrap/Card";
-import Form from "react-bootstrap/Form";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import { getAllUsers } from "../../api/delivery.api";
+import {
+  Button,
+  Alert,
+  Card,
+  Form,
+  Container,
+  Row,
+  Col,
+} from "react-bootstrap";
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
@@ -26,7 +27,7 @@ function LoginCard() {
       });
 
       localStorage.setItem("access_token", response.data.access);
-      localStorage.setItem("refresh_irem", response.data.refresh);
+      localStorage.setItem("refresh_token", response.data.refresh);
       axios.defaults.headers.common[
         "Authorization"
       ] = `Bearer ${response.data.access}`;
@@ -44,93 +45,70 @@ function LoginCard() {
   };
 
   return (
-    <>
-      <div className={styles.Background}>
-        <Container className={styles.FormContainer}>
-          <Row className={styles.RowStyle}>
-            <Col xs={12} md={8} lg={4}>
-              <Card.Body>
-                <Card.Title className={styles.LoginTitle}>
-                  INICIAR SESION
-                </Card.Title>
-                {errorMessage && (
-                  <LoginError message={errorMessage} variant="danger" />
-                )}
-                <Form onSubmit={handleSubmit}>
-                  <UsernameInput
-                    username={username}
-                    setUsername={setUsername}
-                  />
-                  <PasswordInput
-                    password={password}
-                    setPassword={setPassword}
-                  />
-                  <LoginButton></LoginButton>
-                </Form>
-              </Card.Body>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    </>
+    <div className={styles.Background}>
+      <Container className={styles.FormContainer}>
+        <Row className={styles.RowStyle}>
+          <Col xs={12} md={8} lg={4}>
+            <Card.Body>
+              <Card.Title className={styles.LoginTitle}>
+                INICIAR SESION
+              </Card.Title>
+              {errorMessage && (
+                <LoginError message={errorMessage} variant="danger" />
+              )}
+              <Form onSubmit={handleSubmit}>
+                <UsernameInput username={username} setUsername={setUsername} />
+                <PasswordInput password={password} setPassword={setPassword} />
+                <LoginButton></LoginButton>
+              </Form>
+            </Card.Body>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
 function UsernameInput({ username, setUsername }) {
   return (
-    <>
-      <div>
-        <Form.Group controlId="username" className="mb-3">
-          <Form.Label className={styles.LabelStyle}>
-            Nombre de usuario
-          </Form.Label>
-          <Form.Control
-            className={styles.InputStyle}
-            type="username"
-            placeholder="Ingrese su nombre de usuario"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </Form.Group>
-      </div>
-    </>
+    <div>
+      <Form.Group controlId="username" className="mb-3">
+        <Form.Label className={styles.LabelStyle}>Nombre de usuario</Form.Label>
+        <Form.Control
+          className={styles.InputStyle}
+          type="text"
+          placeholder="Ingrese su nombre de usuario"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </Form.Group>
+    </div>
   );
 }
 
 function PasswordInput({ password, setPassword }) {
   return (
-    <>
-      <div>
-        <Form.Group controlId="password" className="mb-3">
-          <Form.Label className={styles.LabelStyle}>Contraseña</Form.Label>
-          <Form.Control
-            className={styles.InputStyle}
-            type="password"
-            placeholder="Ingrese su contraseña"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-      </div>
-    </>
+    <div>
+      <Form.Group controlId="password" className="mb-3">
+        <Form.Label className={styles.LabelStyle}>Contraseña</Form.Label>
+        <Form.Control
+          className={styles.InputStyle}
+          type="password"
+          placeholder="Ingrese su contraseña"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </Form.Group>
+    </div>
   );
 }
 
 function LoginButton() {
-  async function handleClick() {
-    const res = await getAllUsers();
-    console.log(res);
-  }
   const navigate = useNavigate();
   return (
     <>
       <div>
-        <Button
-          className={styles.LoginButton}
-          variant="warning"
-          type="submit"
-          //onClick={() => navigate("/dashboard")}
-        >
+        <Button className={styles.LoginButton} variant="warning" type="submit">
           INICIAR SESIÓN
         </Button>
       </div>
