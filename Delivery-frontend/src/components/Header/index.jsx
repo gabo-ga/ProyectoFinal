e4c010bd1f12843a4b2de225002ea8093cebe6b6
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2024-10-16 23:41:47 -0400

    style:svg user icon with correct height

--- Delivery-frontend/src/components/Header/index.jsx
+++ Delivery-frontend/src/components/Header/index.jsx
@@ -1,5 +1,4 @@
 import Row from "react-bootstrap/esm/Row";
-import UserProfile from "../../assets/user.png";
 import styles from "./header.module.css";
 import Container from "react-bootstrap/esm/Container";
 import Col from "react-bootstrap/esm/Col";
@@ -7,20 +6,18 @@ import { Person, PersonCircle } from "react-bootstrap-icons";
 
 function Header() {
   return (
-    <>
-      <Container fluid className={styles.HeaderStyles}>
-        <Row className="w-100 h-100">
-          <Col xs={12} md={6}></Col>
-          <Col
-            xs={12}
-            md={6}
-            className={`${styles.ProfileContainer} d-flex justify-content-end`}
-          >
-            <PersonCircle></PersonCircle>
-          </Col>
-        </Row>
-      </Container>
-    </>
+    <Container fluid className={styles.HeaderStyles}>
+      <Row className={styles.row}>
+        <Col xs={12} md={6}></Col>
+        <Col
+          xs={12}
+          md={6}
+          className={`${styles.ProfileContainer} d-flex justify-content-end`}
+        >
+          <PersonCircle className={styles.profile}></PersonCircle>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
