c69be75362a852543ce0f72d8eaac53c81cfe20c
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2024-10-20 12:25:17 -0400

    feat:accept and cancel button components

---
+++ Delivery-frontend/src/components/AcceptButton/index.jsx
@@ -0,0 +1,7 @@
+import Button from "react-bootstrap/Button";
+
+function AcceptButton() {
+  return <Button variant="success">Success</Button>;
+}
+
+export default AcceptButton;
---
+++ Delivery-frontend/src/components/CancelButton/index.jsx
@@ -0,0 +1,7 @@
+import Button from "react-bootstrap/Button";
+
+function CancelButton() {
+  return <Button variant="danger">Success</Button>;
+}
+
+export default CancelButton;
