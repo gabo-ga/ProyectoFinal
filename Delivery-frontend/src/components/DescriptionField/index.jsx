feb3c09457a2a6aaa9a4392040d2beb4b14d05e7
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2024-11-19 12:19:20 -0400

    refactor: new components for orderForm

---
+++ Delivery-frontend/src/components/DescriptionField/index.jsx
@@ -0,0 +1,16 @@
+import { Form } from "react-bootstrap";
+
+const DescriptionField = ({ value, onChange }) => (
+  <Form.Group controlId="formDescription">
+    <Form.Label>DESCRIPCIÓN:</Form.Label>
+    <Form.Control
+      as="textarea"
+      value={value}
+      onChange={onChange}
+      name="detalle"
+      rows={3}
+    />
+  </Form.Group>
+);
+
+export default DescriptionField;
