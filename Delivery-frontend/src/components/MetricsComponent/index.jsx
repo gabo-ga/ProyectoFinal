5ab0020c1a0a0aaf158f90d0551ae54e55c1e48b
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2024-11-21 10:57:07 -0400

    feat: new file for queries and 2 endpotins for counting orders and vehicles

--- Delivery-frontend/src/components/MetricsComponent/index.jsx
+++ Delivery-frontend/src/components/MetricsComponent/index.jsx
@@ -1,39 +1,58 @@
+import React, { useEffect, useState } from "react";
 import Container from "react-bootstrap/esm/Container";
-import Card from "react-bootstrap/esm/Card";
-import styles from "./metrics.module.css";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
+import styles from "./metrics.module.css";
+import axiosInstance from "../../axiosInstance";
 
 function Metrics() {
   return (
-    <>
-      <Container className={styles.MetricsContainer}>
-        <MetricsBody></MetricsBody>
-      </Container>
-    </>
+    <Container className={styles.MetricsContainer}>
+      <MetricsBody />
+    </Container>
   );
 }
+
 function MetricsBody() {
+  const [pedidosEnCurso, setPedidosEnCurso] = useState(0);
+  const [vehiculosDisponibles, setVehiculosDisponibles] = useState(0);
+
+  useEffect(() => {
+    // Función para obtener pedidos en curso
+    const fetchPedidosEnCurso = async () => {
+      try {
+        const response = await axiosInstance.get("/api/v1/pedidos/count/");
+        setPedidosEnCurso(response.data.count); // Actualiza el estado con el conteo
+      } catch (error) {
+        console.error("Error al obtener los pedidos en curso:", error);
+      }
+    };
+
+    // Función para obtener vehículos disponibles
+    const fetchVehiculosDisponibles = async () => {
+      try {
+        const response = await axiosInstance.get("/api/v1/vehiculos/count/");
+        setVehiculosDisponibles(response.data.count); // Actualiza el estado con el conteo
+      } catch (error) {
+        console.error("Error al obtener los vehículos disponibles:", error);
+      }
+    };
+
+    fetchPedidosEnCurso();
+    fetchVehiculosDisponibles();
+  }, []); // Ejecuta ambas llamadas solo una vez al montar el componente
+
   return (
-    <>
-      <Row>
-        <Col xs={12} md={4}>
-          <Card.Title className={styles.TextStyle}>
-            Pedidos en curso:
-          </Card.Title>
-        </Col>
-        <Col xs={12} md={4}>
-          <Card.Title className={styles.TextStyle}>
-            Vehículos Disponibles:
-          </Card.Title>
-        </Col>
-        <Col xs={12} md={4}>
-          <Card.Title className={styles.TextStyle}>
-            Tiempo promedio de entrega:
-          </Card.Title>
-        </Col>
-      </Row>
-    </>
+    <Row>
+      <Col xs={12} md={6}>
+        <h3 className={styles.TextStyle}>Pedidos en curso: {pedidosEnCurso}</h3>
+      </Col>
+      <Col xs={12} md={6}>
+        <h3 className={styles.TextStyle}>
+          Vehículos Disponibles: {vehiculosDisponibles}
+        </h3>
+      </Col>
+    </Row>
   );
 }
 
