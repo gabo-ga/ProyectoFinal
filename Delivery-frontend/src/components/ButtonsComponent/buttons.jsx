d151acc04bc326590d8099266412e9359a77602f
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2024-11-22 14:34:06 -0400

    feat:link to metrics page

--- Delivery-frontend/src/components/ButtonsComponent/buttons.jsx
+++ Delivery-frontend/src/components/ButtonsComponent/buttons.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import styles from "./buttons.module.css";
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
@@ -5,24 +6,37 @@ import Col from "react-bootstrap/esm/Col";
 
 function ButtonsContainer() {
   return (
-    <>
-      <Container>
-        <Row className={styles.ButtonsContainer}>
-          <Col xs={12} md={12}>
-            <Button
-              text={"Analisis de datos"}
-              className={styles.TextStyle}
-            ></Button>
-          </Col>
-        </Row>
-      </Container>
-    </>
+    <Container>
+      <Row className={styles.ButtonsContainer}>
+        <Col xs={12} md={12}>
+          <Button
+            text={"Análisis de datos"}
+            className={styles.TextStyle}
+            redirectTo="/metrics"
+          />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
 function Button(props) {
-  const { text } = props;
-  return <div className={styles.TextStyle}>{text}</div>;
+  const { text, redirectTo } = props;
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(redirectTo); // Redirige a la página especificada
+  };
+
+  return (
+    <div
+      className={styles.TextStyle}
+      onClick={handleClick}
+      style={{ cursor: "pointer" }}
+    >
+      {text}
+    </div>
+  );
 }
 
 export default ButtonsContainer;
