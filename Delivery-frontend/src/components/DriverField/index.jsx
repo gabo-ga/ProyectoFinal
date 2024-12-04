2c0de4b885e12cf9fbb81a9b6facbd17720b3347
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2024-12-04 17:06:43 -0400

    fix:validation in driver field

--- Delivery-frontend/src/components/DriverField/index.jsx
+++ Delivery-frontend/src/components/DriverField/index.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { Form } from "react-bootstrap";
+import PropTypes from "prop-types";
 import { fetchDriversWithActiveOrders } from "../../api/apiService";
 
 const DriverField = ({ value, onChange }) => {
   const [drivers, setDrivers] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     const cargarConductores = async () => {
@@ -21,13 +23,29 @@ const DriverField = ({ value, onChange }) => {
     cargarConductores();
   }, []);
 
+  const handleChange = (e) => {
+    const selectedValue = e.target.value;
+
+    if (selectedValue === "") {
+      setError("Por favor, seleccione un conductor.");
+    } else {
+      setError("");
+    }
+    onChange(e);
+  };
+
   return (
     <Form.Group controlId="formDesignatedDriver">
       <Form.Label>CONDUCTOR DESIGNADO:</Form.Label>
       {isLoading ? (
         <Form.Text>Cargando conductores...</Form.Text>
       ) : (
-        <Form.Select name="conductor" value={value} onChange={onChange}>
+        <Form.Select
+          name="conductor"
+          value={value}
+          onChange={onChange}
+          required
+        >
           <option value="">Seleccione un conductor</option>
           {drivers.map((driver) => (
             <option key={driver.id} value={driver.id}>
@@ -38,6 +56,12 @@ const DriverField = ({ value, onChange }) => {
       )}
     </Form.Group>
   );
+};
+
+DriverField.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
 };
 
 export default DriverField;
