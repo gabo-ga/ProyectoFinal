f820a6e23319b754234771715ffd597be33089c7
Gabriel Gonzales <gabo.gonzales1801@gmail.com>
2025-01-23 17:28:51 -0400

    feat: actualizar endpoint de inicio de sesión para autenticación con JWT

--- Delivery/AngusEats/urls.py
+++ Delivery/AngusEats/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
-    #endpoint nuevo para autenticacion sin JWT
+    #endpoint nuevo para autenticacion con JWT
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LoginView.as_view(), name='logout'),    
     
