f09737108f67f475df1eef300178a009dc2cc77c
Gabriel Gonzales <gabriel.gonzales1801@gmail.com>
2025-03-22 11:00:06 -0400

    footer styling with tailwind

--- Delivery-frontend/src/layout/Footer/index.jsx
+++ Delivery-frontend/src/layout/Footer/index.jsx
@@ -1,12 +1,9 @@
-import Container from "react-bootstrap/esm/Container";
-import styles from "./footer.module.css";
+import React from "react";
 
 function Footer() {
   return (
-    <footer className={styles.FooterStyle}>
-      <Container fluid className={styles.FooterStyle}>
-        
-      </Container>
+    <footer className="flex w-full h-2 justify-center items-center bg-[#1abc9c] absolute p-4 md:h-4 md:p-8">
+      
     </footer>
   );
 }
