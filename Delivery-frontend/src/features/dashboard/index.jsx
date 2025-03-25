0596bf38bf375dec110e490bf1bc17f5cfd393c5
Gabriel Gonzales <gabriel.gonzales1801@gmail.com>
2025-03-25 13:21:30 -0400

    feat:dashboard with tailwind

--- Delivery-frontend/src/features/dashboard/index.jsx
+++ Delivery-frontend/src/features/dashboard/index.jsx
@@ -9,28 +9,21 @@ import Metrics from "../../components/MetricsComponent";
 import CompletedOrders from "../../components/OrdersComponents/completedOrders";
 import ActiveVehicles from "../../components/OrdersComponents/activeVehicles";
 
-import Container from "react-bootstrap/esm/Container";
-import Row from "react-bootstrap/esm/Row";
-import Col from "react-bootstrap/esm/Col";
-
 function Dashboard() {
   return (
     <>
       <Header></Header>
-      <main className="bg-[#ecf0f1] h-auto p-4 gap-2 flex flex-col">
-        <section className="grid grid-rows-7 gap-2 h-auto">
+      <main className="bg-[#ecf0f1] h-auto p-4 gap-2 flex flex-col md:flex-row lg:flex-row">
+        <section className="w-full grid grid-rows-7 gap-2 h-auto md:w-full lg:w-1/2">
             <Map></Map>
             <Metrics></Metrics>
             <ButtonsContainer></ButtonsContainer>
             </section>
-            <section>
-          
+            <section className="flex flex-col md:w-full lg:w-1/2">
             <ActiveOrders title={"PEDIDOS EN CURSO"}></ActiveOrders>
             <CompletedOrders title={"PEDIDOS COMPLETADOS"}></CompletedOrders>
             <ActiveVehicles title={"VEHICULOS ACTIVOS"}></ActiveVehicles>
             </section>
-        
-      
       </main>
       <Footer></Footer>
     </>
