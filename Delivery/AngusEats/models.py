e3f982e7c136eca42dfacb51235d348246b59d14
Gabriel Gonzales <gabriel.gonzales1801@gmail.com>
2025-07-01 18:26:28 -0400

    models file merged

--- Delivery/AngusEats/models.py
+++ Delivery/AngusEats/models.py
@@ -2,8 +2,31 @@ from django.db import models
 from django.contrib.gis.db import models as gis_models
 from django.contrib.postgres.operations import CreateExtension
 from django.core.validators import RegexValidator
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+class UsuarioManager(BaseUserManager):
+    def create_user(self, usuario, nombre, telefono, password=None, **extra_fields):
+        if not usuario:
+            raise ValueError("El campo 'usuario' es obligatorio.")
+        if not telefono:
+            raise ValueError("El campo 'telefono' es obligatorio.")
+        
+        extra_fields.setdefault('is_active', True)
+        user = self.model(usuario=usuario, nombre=nombre, telefono=telefono, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, usuario, nombre, telefono, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('rol', 'admin')
+        return self.create_user(usuario, nombre, telefono, password, **extra_fields)
+
+
+
 # Create your models here.
-class Usuario(models.Model):
+class Usuario(AbstractBaseUser, PermissionsMixin):
     ROL_CHOICES = [
         ('admin', 'Admin'),
         ('conductor', 'Conductor')
@@ -12,13 +35,19 @@ class Usuario(models.Model):
     nombre = models.CharField(max_length=100)
     usuario = models.CharField(max_length=150, unique=True, null=True)
     correo = models.EmailField(unique=True)
-    contrasena_hash = models.CharField(max_length=64)
     rol = models.CharField(max_length=50, choices=ROL_CHOICES)
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     telefono = models.CharField(max_length=15, null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    
+    USERNAME_FIELD = 'usuario'
+    REQUIRED_FIELDS = ['nombre', 'correo', 'telefono']
+    
+    objects = UsuarioManager()
     
     def __str__(self):
-        return self.nombre  
+        return f"{self.nombre} ({self.rol})"  
 
 class Cliente(models.Model):
         nombre = models.CharField(max_length=100)
@@ -100,19 +129,13 @@ class Vehiculo(models.Model):
     def __str__(self):
         return self.vehiculo_nombre
 
-    
-class HistorialGPS(models.Model):
-        vehiculo = models.ForeignKey('Vehiculo', on_delete=models.CASCADE)
-        posicion = gis_models.PointField(srid=4326)
-        marca_tiempo = models.DateTimeField(auto_now_add=False)
-    
-        def __str__(self):
-                return f"Historial GPS {self.id} - Vehiculo {self.vehiculo}"
+
 
 
 class AnalisisPedido(models.Model):
     pedidos_totales = models.IntegerField()
     pedidos_entregados = models.IntegerField()
+    pedidos_cancelados = models.IntegerField(default=0)
     tiempo_promedio_entrega_minutos = models.IntegerField(null=True, blank=True)
     kilometros_recorridos_totales = models.DecimalField(max_digits=10, decimal_places=2)
     fecha_analisis = models.DateTimeField(auto_now_add=True)
@@ -121,7 +144,6 @@ class AnalisisPedido(models.Model):
         return f"Analisis {self.id} = {self.fecha_analisis}"
     
 #Model para la configuracion de direccino origen
-# models.py
 class Configuracion(models.Model):
     direccion_origen = models.CharField(max_length=255, blank=True, null=True) 
     punto_origen = gis_models.PointField(srid=4326, null=True, blank=True)
