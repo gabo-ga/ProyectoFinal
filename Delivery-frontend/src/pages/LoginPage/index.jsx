40dd365e7e1813470aed3190a8de07aea97eef88
Gabriel Gonzales <gabriel.gonzales1801@gmail.com>
2025-07-01 18:20:19 -0400

    fix:background color changed

--- Delivery-frontend/src/pages/LoginPage/index.jsx
+++ Delivery-frontend/src/pages/LoginPage/index.jsx
@@ -36,7 +36,7 @@ function Login() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-[#1abc9c] flex items-center justify-center px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <div className="bg-white p-8 rounded-lg shadow-md">
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
